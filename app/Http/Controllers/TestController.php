73e72cd3f1fef4564245daca59b2bb0e7340d36d
DJ-phi <94509379+DJ-phi@users.noreply.github.com>
2022-10-26 02:37:50 +0900

    コントローラの処理 index

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,12 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Test;
 
 class TestController extends Controller
 {
     // indexの処理
     public function index ()
     {
-        return view( "tests.test" ); //view( "フォルダ名.ファイル名" );
+        //railsのUser.allと一緒
+        $values = Test::all();
+
+        // 処理を止めて内容を確認できる
+        // railsのconsol + User/allでレコードが出ている状態
+        // dd( $values );
+
+        // compact()関数で渡すと楽 compactの中身の変数は$がいらなくなる
+        return view( "tests.test", compact( "values" ) ); //view( "フォルダ名.ファイル名" );
     }
 }
