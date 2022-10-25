405979a0149385c87f778b4dbb8422d319bc788b
DJ-phi <94509379+DJ-phi@users.noreply.github.com>
2022-10-26 01:41:37 +0900

    コントローラ indexの処理

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
-    //
+    // indexの処理
+    public function index ()
+    {
+        return view( "tests.test" ); //view( "フォルダ名.ファイル名" );
+    }
 }
