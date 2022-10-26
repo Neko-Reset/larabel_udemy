c2b0395095dac7fd09a04b21b5d398af1f787f3e
DJ-phi <94509379+DJ-phi@users.noreply.github.com>
2022-10-26 13:26:32 +0900

    エロクアントとコレクション型

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -10,12 +10,20 @@ class TestController extends Controller
     // indexの処理
     public function index ()
     {
-        //railsのUser.allと一緒
-        $values = Test::all();
+        // railsのUser.allと一緒
+        // Eloquent(エロクアント)
+        $values = Test::all(); //Eloquent/Collection
 
+        $count = Test::count(); //数字
+
+        $first = Test::findOrFail(1); //インスタンス
+
+        $whereBBB = Test::where("test", "=", "bb"); //Eloquent/Builder
+        $whereBB = Test::where("test", "=", "bb")->get(); //Collection
         // 処理を止めて内容を確認できる
         // railsのconsol + User/allでレコードが出ている状態
         // dd( $values );
+        dd($values, $count,$first,$whereBBB);
 
         // compact()関数で渡すと楽 compactの中身の変数は$がいらなくなる
         return view( "tests.test", compact( "values" ) ); //view( "フォルダ名.ファイル名" );
